fa15241a58797c00b7c96d9a89d356472816c891
“Owusu <owusuachamfour@gmail.com>
2023-09-29 06:14:19 -0400

    started controller file

---
+++ controllers/logsController.js
@@ -0,0 +1,7 @@
+//initialize controller
+const express = require("express");
+const router = express.Router();
+const Log = require("../models/logs.js");
+
+//export router object
+module.exports = router;
